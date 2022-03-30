a0d74733937884fcab910de7b95b89276dc56792
Michal Vasilek <michal@vasilek.cz>
2022-03-30 18:33:39 +0200

    xbps-src: remove duplicate entries

--- common/xbps-src/shutils/bulk.sh
+++ common/xbps-src/shutils/bulk.sh
@@ -68,7 +68,7 @@ bulk_build() {
 
     # Compare installed pkg versions vs srcpkgs
     if [[ $sys ]]; then
-        xbps-checkvers -f '%n' -I -D $XBPS_DISTDIR
+        xbps-checkvers -f '%n' -I -D $XBPS_DISTDIR | sort -u
         return $?
     fi
     # compare repo pkg versions vs srcpkgs
